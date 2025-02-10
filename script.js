7c5c4b3f0e933486cdfc6158738ae7230e881110
raj-45 <rajnishkumarg123@gmail.com>
2025-02-11 02:14:07 +0530

    Using IP

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function uploadFile() {
     };
 
     try {
-      const response = await fetch("http://localhost:3000/upload", {
+      const response = await fetch("http://103.175.180.62:3000/upload", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
